1e6501ee3a21d8352d76f0b40f25710b8d671621
Andres Arturo Rodriguez Calderon <57767201+andresrodriguez55@users.noreply.github.com>
2022-03-20 01:46:44 +0300

    Delete Flutter/chatapp directory

--- Flutter/chatapp/android/app/src/main/kotlin/com/example/chatapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.chatapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Flutter/chatapp/lib/ServerData.dart
+++
@@ -1,5 +0,0 @@
-class ServerData
-{
-  final String SERVER_URL="http://10.0.2.2:8000";
-  final String SERVER_IP="10.0.2.2:8000";
-}
